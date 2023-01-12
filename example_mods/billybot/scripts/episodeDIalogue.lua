b2353a5b02223a0ced667e933c704ab658e30874
AydenTheMusician <119458242+AydenTheMusician@users.noreply.github.com>
2023-01-12 01:51:53 -0500

    EPISODECARD SHITS

---
+++ example_mods/billybot/scripts/episodeDIalogue.lua
@@ -0,0 +1,32 @@
+local epiShit = "images/episodeshits"
+
+createImage(epiShit, "bg", addImage, 0,90)
+addImage("bg", 0,90, setScrollFactor(19,100))
+
+createImage(epiShit, "billyRobot", addImage, 130,560)
+addImage("billyRobot", add)
+
+createImage(epiShit, "episodeTitle", addImage, middle, Top)
+addImage("episodeTItle", createImage)
+
+createImage(epiShit, "mandy", addImage, 100,800)
+addImage("bg", 0,90, setScrollFactor)
+
+if StoryMode == then
+{
+    epiShit.visble = true
+    epiShit.episodeDial
+
+    episodeDial.play:(0.65,. 1)
+
+    psychDefaultFade.fadeOut = true
+
+    wait(11) then
+    {
+        startSong("data/billybot", songStartUp) = true
+    }
+
+    
+
+    print("loaded up episode card")
+}
